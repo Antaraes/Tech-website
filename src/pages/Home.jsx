8bbf63b7baf1eb53ba527251bce4776ed5dd7f08
yawai990 <ywaung990@gmail.com>
2023-01-16 10:28:09 +0630

    layout

---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div className='underline text-blue-500'>
+        <h3 className='text-red-400'>Hello from Our E-commerce App, Welcome from Hell</h3>
+    </div>
+  )
+}
+
+export default Home
